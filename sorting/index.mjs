6090879454869db9de98ea405c7e155eb8bb80fd
Subhash Jha <subhash.983@gmail.com>
2018-12-16 10:58:32 +0530

    bubble sort

---
+++ sorting/index.mjs
@@ -0,0 +1,19 @@
+const bubbleSort = arr => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let swapped = false;
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        swapped = true;
+      }
+    }
+    if (!swapped) {
+      break;
+    }
+  }
+  return arr;
+};
+
+console.log(bubbleSort([5, 1, 6, 3, 4, 12, 5, 6, 4, -2]));
