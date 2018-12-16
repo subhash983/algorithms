2e29b8187d910c3bcd6ef442573ab94425dde880
Subhash Jha <subhash.983@gmail.com>
2018-12-16 23:23:41 +0530

    adding selection sort

--- sorting/index.mjs
+++ sorting/index.mjs
@@ -16,4 +16,23 @@ const bubbleSort = arr => {
   return arr;
 };
 
-console.log(bubbleSort([5, 1, 6, 3, 4, 12, 5, 6, 4, -2]));
+//console.log(bubbleSort([5, 1, 6, 3, 4, 12, 5, 6, 4, -2]));
+
+const selectionSort = arr => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let indexOfMin = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[indexOfMin]) {
+        indexOfMin = j;
+      }
+    }
+    if (indexOfMin !== i) {
+      let temp = arr[i];
+      arr[i] = arr[indexOfMin];
+      arr[indexOfMin] = temp;
+    }
+  }
+  return arr;
+};
+
+console.log(selectionSort([5, 1, 6, 3, 4, 12, 5, 6, 4, -2]));
