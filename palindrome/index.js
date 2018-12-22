cb9837d7f6beece9baf31fecb80845639d52d80a
Subhash Jha <subhash.983@gmail.com>
2018-12-23 00:08:25 +0530

    another palindrome

--- palindrome/index.js
+++ palindrome/index.js
@@ -1,6 +1,19 @@
 const isPalindrome = str => {
-  let reveresedString = str.split("").reverse().join("");
+  let reveresedString = str
+    .split("")
+    .reverse()
+    .join("");
   return str === reveresedString;
 };
 
-console.log(isPalindrome('malyylam'));
+const isPalindromeAnother = str => {
+  let strArray = str.split("");
+  for (let i = 0; i < Math.ceil(strArray.length / 2); i++) {
+    if (strArray[i] !== strArray[strArray.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(isPalindromeAnother("malyylam"));
