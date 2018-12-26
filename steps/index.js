e5a8330fd94d391ba4aa1df2c97b5529f3c58aad
Subhash Jha <subhash.983@gmail.com>
2018-12-27 00:44:48 +0530

    steps recursion

--- steps/index.js
+++ steps/index.js
@@ -1,16 +1,28 @@
 const printSteps = n => {
   let nLengthString = "";
   for (let i = 0; i < n; i++) {
-    nLengthString += "a";
+    nLengthString += " ";
   }
-  console.log(nLengthString.length);
   for (let i = 0; i < n; i++) {
-    let firsPart = nLengthString.slice(0, i + 1);
-    console.log(firsPart);
-    firsPart = firsPart.replace("a", "#");
+    let firsPart = nLengthString.slice(0, i + 1).replace(/\s/g, "#");
     let secondPart = nLengthString.slice(i + 1);
     console.log(firsPart + secondPart);
   }
 };
 
-printSteps(2);
+const printStepsRecursion = (n, row = 0, stair = "") => {
+  if (row === n) {
+    return;
+  }
+  if (stair.length === n) {
+    console.log(stair);
+    printStepsRecursion(n, row + 1);
+  }
+  if (stair.length <= row) {
+    stair += "#";
+  } else {
+    stair += " ";
+  }
+  steps(n, row, stair);
+};
+printSteps(4);
