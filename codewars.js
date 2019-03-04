afd1e1e27fb44ac85e4fb9a358fb0e2f4f7b1c96
Subhash Jha <subhash.983@gmail.com>
2019-03-04 23:57:23 +0530

    I Spy problem

--- codewars.js
+++ codewars.js
@@ -11,6 +11,38 @@ const deleteNth = (arr, n) => {
   });
 }
 
-console.log(deleteNth([
-  1, 1, 1, 1
-], 2));
+// console.log(deleteNth([
+//   1, 1, 1, 1
+// ], 2));
+
+
+//I Spy
+const spyOn = func =>
+{
+  let callCount = 0;
+  let argsList = [];
+  let returnedVals = [];
+  let spyOnFunc = function(...args){
+    callCount++;
+    argsList.push(...args);
+    let retVal =  func(...args);
+    returnedVals.push(retVal);
+    return retVal;
+  }
+  spyOnFunc.callCount = function(){
+    return callCount;
+  }
+  spyOnFunc.wasCalledWith = function(val){
+    return argsList.includes(val);
+  }
+  spyOnFunc.returned = function(val){
+    return returnedVals.includes(val);
+  }
+  return spyOnFunc;
+}
+
+function adder(n1, n2) { return n1 + n2; }
+var adderSpy = spyOn(adder);
+console.log(adderSpy(3, 5));
+console.log(adderSpy(2, 4));
+console.log(adderSpy.callCount());
