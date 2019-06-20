3ef6b616b9cb3382b1391a79fd767046d3c64ddd
Subhash Jha <subhash.983@gmail.com>
2019-06-20 07:42:46 +0530

    MongoDb ObjectID Validation

--- codewars.js
+++ codewars.js
@@ -73,4 +73,34 @@ const tour = (friends, fTowns, distTable) => {
 var friends1 = ["A1", "A2", "A3", "A4", "A5"];
 var fTowns1 = [["A1", "X1"], ["A2", "X2"], ["A3", "X3"], ["A4", "X4"]];
 var distTable1 = ["X1", 100.0, "X2", 200.0, "X3", 250.0, "X4", 300.0];
-console.log(tour(friends1, fTowns1, distTable1));
+//console.log(tour(friends1, fTowns1, distTable1));
+
+//MongoDB ObjectID
+const Mongo = {
+  isValid: function(s) {
+    return false;
+  },
+  getTimestamp: function(s) {
+    //getting first 4 bytes which contains timestamp
+    let hexadecimalMap = {
+      a: 10,
+      b: 11,
+      c: 12,
+      d: 13,
+      e: 14,
+      f: 15
+    };
+    let dateHexString = s.substr(0, 8);
+    let dateHexStringArr = dateHexString.split("");
+    let index = 7;
+    let decimalEquivalent = 0;
+    for (const hexChar of dateHexStringArr) {
+      decimalEquivalent +=
+        (hexadecimalMap[hexChar] || hexChar) * Math.pow(16, index--);
+    }
+    return new Date(decimalEquivalent * 1000);
+  }
+};
+
+console.log(Mongo.getTimestamp('507f1f77bcf86cd799439011'));
+
